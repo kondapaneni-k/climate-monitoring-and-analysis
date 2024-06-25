fdbdd4b6323badcbd3674961a51f97967036ab37
Keerthi_Kondapaneni <122726421+kondapaneni-k@users.noreply.github.com>
2024-06-25 15:00:32 +0530

    ahalya.py

--- Software Engineering Mini Project/ahalya.py
+++ Software Engineering Mini Project/ahalya.py
@@ -5,9 +5,9 @@ df=pd.read_csv("C:\\Users\\keert\\OneDrive\\Desktop\\templates\\Indian_cities.cs
 l1=["None","Temperature","Humidity","Wind Speed","Precipitation"]
 city=st.selectbox("Select Location",[city for city in df])
 year=st.selectbox("Select year",[year for year in range(2013,2023)])
-p1=st.selectbox("Seelct Parameter1",l1)
-p2=st.selectbox("Seelct Parameter2",l1)
-p3=st.selectbox("Seelct Parameter3",l1)
-p4=st.selectbox("Seelct Parameter4",l1)
+p1=st.selectbox("Select Parameter1",l1)
+p2=st.selectbox("Select Parameter2",l1)
+p3=st.selectbox("Select Parameter3",l1)
+p4=st.selectbox("Select Parameter4",l1)
 submitted=st.button("Submit")
 
